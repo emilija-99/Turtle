80990fb415886246c309aceac96821e3680fb2da
emilija-99 <emaristic53@gmail.com>
2024-09-21 16:45:31 +0200

    added loader component and animation

--- Turtle_/src/app/pages/welcome/welcome.component.ts
+++ Turtle_/src/app/pages/welcome/welcome.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { LoaderComponent } from "../../components/loader/loader.component";
+import { Subscriber, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [],
+  imports: [LoaderComponent],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit, AfterViewInit{
+  
+  public isLoading:boolean = true;
+  private loaderClear:ReturnType<typeof setTimeout> | undefined;
+ 
+
+  ngAfterViewInit(): void {
+    console.log("ready!");
+    this.loaderClear = setTimeout(() => {
+      console.log('loading');
+      this.isLoading = false;
+    }, 1000);
+
+    console.log("this.isLoadig: ", this.isLoading);
+  }
+ 
+  ngOnInit(): void {
+   
+    if(this.loaderClear) clearTimeout(this.loaderClear)
+  }
+
+  
 
 }
